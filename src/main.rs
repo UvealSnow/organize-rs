366c89a8e276e19cc92c2a19be731e9c0746956a
Kevin Avila <hola@kevinavila.pro>
2025-04-16 13:24:58 +0200

    mover feat: Enhance main logic to support verbose output and write changelog to file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use config::Config;
 use mover::Mover;
 use reader::Reader;
-use std::env;
+use std::{env, fs};
 
 mod config;
 mod mover;
@@ -11,12 +11,23 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(args).expect("Failed to parse command line arguments");
 
+    let target_dir = config.clone().target_dir;
     let mover = Mover::new(Reader::new(config.target_dir, config.exclude).read_folder());
-
     let changes = mover.generate_change_log().join("\n");
-    println!("{}", changes);
 
     if config.is_dry_run {
         return;
+    }
+
+    if config.is_verbose {
+        println!("{}", changes);
+    }
+
+    if let Err(e) = mover.do_move_files() {
+        println!("Failed with error {}", e);
+    }
+
+    if let Err(e) = fs::write(target_dir.join("changelog.txt"), changes) {
+        println!("Failed to write changelog: {}", e);
     }
 }
