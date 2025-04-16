ae7f9d2908aa989f316c3534de77846a2fdff5e1
Kevin Avila <hola@kevinavila.pro>
2025-04-16 11:58:31 +0200

    mover refactor: changed loop variable name

--- src/mover/mover.rs
+++ src/mover/mover.rs
@@ -14,13 +14,13 @@ impl Mover {
     pub fn generate_change_log(&self) -> Vec<String> {
         let mut result: Vec<String> = vec![];
 
-        for entity in &self.files_to_move {
-            let file_name = entity.get_file_name();
+        for entry in &self.files_to_move {
+            let file_name = entry.get_file_name();
             result.push(format!(
                 "{}/{} => {}",
-                entity.get_current_path(),
+                entry.get_current_path(),
                 file_name,
-                entity.get_new_path(),
+                entry.get_new_path(),
             ));
         }
 
