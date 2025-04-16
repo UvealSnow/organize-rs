7ee1f630b8e48278c879e9c702dd97b1df5e6e94
Kevin Avila <hola@kevinavila.pro>
2025-04-16 11:29:10 +0200

    config feat: Implement Config struct for handling application configuration

---
+++ src/config/config.rs
@@ -0,0 +1,53 @@
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub struct Config {
+    pub exclude: Vec<String>,
+    pub target_dir: PathBuf,
+    config_file_path: Option<PathBuf>,
+    pub is_dry_run: bool,
+    pub is_stats_run: bool,
+}
+
+impl Config {
+    pub fn new(args: Vec<String>) -> Result<Self, String> {
+        let mut exclude = Vec::new();
+        let mut config_file_path = None;
+        let mut is_dry_run = false;
+        let mut is_stats_run = false;
+
+        let mut iter = args.into_iter().skip(1);
+        let target_dir = match iter.next() {
+            None => PathBuf::from("."),
+            Some(t) => PathBuf::from(t),
+        };
+
+        while let Some(arg) = iter.next() {
+            match arg.as_str() {
+                "--dry-run" => is_dry_run = true,
+                "--stats" => is_stats_run = true,
+                "--exclude" => {
+                    let patterns = iter
+                        .next()
+                        .ok_or("--exclude requires a pattern (e.g. '*.rs,*.toml').")?;
+                    exclude.extend(patterns.split(",").map(|s| s.to_string()));
+                }
+                "--config" => {
+                    let path_as_string = iter
+                        .next()
+                        .ok_or("--config requires a path to a config file.")?;
+                    config_file_path = Some(PathBuf::from(path_as_string));
+                }
+                _ => return Err(format!("unknown argument: {}", arg)),
+            }
+        }
+
+        Ok(Config {
+            exclude,
+            target_dir,
+            config_file_path,
+            is_dry_run,
+            is_stats_run,
+        })
+    }
+}
